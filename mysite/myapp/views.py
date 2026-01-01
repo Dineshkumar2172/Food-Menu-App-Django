f1e7f80cfc5d423734698dda13eca17f6d9d3e92
Dineshkumar2172 <dinesh2000.data@gmail.com>
2026-01-02 00:19:17 +0530

    models, ORM and queryset

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from myapp.models import Item
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
 
 def item(request):
-    return HttpResponse("<h1>This is an item view<h1>")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
