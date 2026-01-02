b177104b102b7a431eabf1323ca2b25b82fd85ec
Dineshkumar2172 <dinesh2000.data@gmail.com>
2026-01-02 23:15:19 +0530

    namespaces

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
 
+# assigning a namespace to prevent name conflict in url names when multiple apps are involved in our project
+app_name = 'myapp'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # naming url - to replace hardcoded urls from being used within our project
     path('item/', views.item),
     path('<int:id>/', views.detail, name='detail')
 ]
