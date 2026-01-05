af044c487b9b440c9f09a8bac6bfa0b7bfe2de42
Dineshkumar2172 <dinesh2000.data@gmail.com>
2026-01-05 10:25:37 +0530

    created middleware to block IP

--- mysite/myapp/middleware.py
+++ mysite/myapp/middleware.py
@@ -1,4 +1,5 @@
 import time
+from django.http import HttpResponseForbidden
 
 class LogRequestMiddleware:
     def __init__(self, get_response):
@@ -16,10 +17,23 @@ class LogRequestMiddleware:
 class TimerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        
+
     def __call__(self, request):
         start = time.time()
         response = self.get_response(request)
         duration = time.time() - start
         print(f"[Middleware] Request took {duration:.2f} seconds")
         return response
+    
+
+class BlockIPMiddleware:
+    BLOCKED_IPS = []
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        ip = request.META.get("REMOTE_ADDR")
+        if ip in self.BLOCKED_IPS:
+            return HttpResponseForbidden("Your IP is blocked")
+
+        return self.get_response(request)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "myapp.middleware.LogRequestMiddleware",
     "myapp.middleware.TimerMiddleware",
+    "myapp.middleware.BlockIPMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
